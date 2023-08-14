7abb457cd2995eefac1b25b4b460e5e06a44f386
Arc Envoy <102623605+devenvoy@users.noreply.github.com>
2023-08-14 19:24:37 +0530

    comment

---
+++ app/src/main/java/com/example/shayari2023/loginpage.java
@@ -0,0 +1,34 @@
+package com.example.shayari2023;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loginpage extends AppCompatActivity {
+
+    private AwesomeValidation awesomeValidation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+
+
+    }
+
+
+}
+
+
+
+
